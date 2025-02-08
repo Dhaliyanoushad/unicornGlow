7b7c0b128f88ae1db64f9a505226e919f80263ce
rasharahim <fathima.rasharahim@gmail.com>
2025-02-08 23:36:20 +0530

    input set()

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import Input from "./components/Input/Input";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import SparklingHeading from "./components/Title/SparklingHead";
 
 function App() {
   return (
     <div>
-      <Hero />
-      <Header />
+      <Header/>
+      <SparklingHeading/>
+      <Hero/>
+      <Input/>
     </div>
   );
 }
