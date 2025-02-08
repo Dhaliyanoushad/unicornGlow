96e8b16cd5e797b53f58f85d16e7b5f7900ea16f
Daliya <daliyadeutsch@gmail.com>
2025-02-09 02:59:53 +0530

    blaaaaaah

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Waves from "./components/Waves/Waves";
+import SparklingHeading from "./components/Title/SparklingHead";
+import ChatBot from "./components/ChatBot/ChatBot";
+import About from "./components/About/About";
+import Mood from "./components/Mood/Mood";
+import Tracker from "./components/Tracker/Tracker";
 
 const App = () => {
   return (
@@ -10,7 +16,9 @@ const App = () => {
       <SparklingHeading />
       <Waves />
       <Hero />
+      <About />
       <ChatBot />
+      <Tracker />
     </>
   );
 };
