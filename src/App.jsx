da0689bd5c21f7a5f0a51b03e8a5704a66920855
Daliya <daliyadeutsch@gmail.com>
2025-02-09 01:19:04 +0530

    waves

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import ChatBot from "./components/ChatBot/ChatBot";
 import SparklingHeading from "./components/Title/SparklingHead";
+import Waves from "./components/Waves/Waves";
 
 function App() {
   return (
     <>
       <Header />
       <SparklingHeading />
+      <Waves />
       <Hero />
       <ChatBot />
     </>
