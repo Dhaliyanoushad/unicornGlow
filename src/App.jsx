115cfb233bab8b76f28ae1608ffa5e4c74d0dc11
Daliya <daliyadeutsch@gmail.com>
2025-02-09 02:42:30 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
@@ -13,6 +13,6 @@ function App() {
       <ChatBot />
     </>
   );
-}
+};
 
 export default App;
