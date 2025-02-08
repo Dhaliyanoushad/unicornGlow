c6387c2245cb1952656bc0c27ac5073836894e9c
Ranjana <kpranjana51@gmail.com>
2025-02-09 01:22:08 +0530

    app.jsx comment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+/*import About from "./components/About/About";
+import Mood from "./components/Mood/Mood";
+import MoodSongs from "./components/MoodSongs/MoodSongs";*/
 
 function App() {
   return (
